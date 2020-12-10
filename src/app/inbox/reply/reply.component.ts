f56403fa1ccd0de8182ee1ebaee467e976c23c93
lifeoflikhon <mehedi.hasansjs@gmail.com>
2020-12-10 21:29:06 +0600

    reply done

--- src/app/inbox/reply/reply.component.ts
+++ src/app/inbox/reply/reply.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Email } from '../models/email';
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-reply',
   templateUrl: './reply.component.html',
   styleUrls: ['./reply.component.scss']
 })
-export class ReplyComponent implements OnInit {
+export class ReplyComponent implements OnChanges {
+  showModal: boolean;
+  @Input() email: Email;
 
-  constructor() { }
+  constructor(private emailService: EmailService) { }
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    let text;
+    if ( this.email.text ) {
+      text = this.email.text.replace(/\n/gi, '\n> ');
+    } else {
+      text = this.email.html;
+    }
+    this.email = {
+      ...this.email,
+      to: this.email.from,
+      from: this.email.to,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n\n------ ${this.email.from} wrote:\n\t> ${text}`
+    };
   }
 
+  onSubmit( email: Email ): void {
+    this.emailService.sendEmail(email).subscribe(() => {
+      this.showModal = false;
+    });
+  }
 }
